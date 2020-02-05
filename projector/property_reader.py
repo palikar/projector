b0bdb05662ea96f14f741f0a7517bb79068e95ec
Stanislav Arnaudov <stanislav_ts@abv.bg>
2020-02-06 00:44:20 +0100

    Doing some work on the reader system

--- projector/property_reader.py
+++ projector/property_reader.py
@@ -20,6 +20,52 @@ class PropertyReader:
             print("Inconsisten property: " + prop["name"])
             sys.exit(1)
 
+
+    def _process_binary(default):
+        value = None
+        while value is None:
+            if default in ("n", "y"):
+                value = input(prop["name"] + "[y/n]("+default+") :")
+                if value.stip() == "":
+                    value = default
+            else:
+                value = input(prop["name"] + "[y/n]:")
+            if value.lower() not in ("y", "n", "yes", "no" ):
+                value = None
+                print("Please type \'y\' or \'n\' ")
+        return value == "y" or value == "yes"
+
+    def _process_choices(defaul, choices):
+        value = None
+        while value is None:
+            if default in choices:
+                value = input(prop["name"]
+                              + "\n" + str(choices)
+                              + "\n(" + default
+                              + ")\n:")
+                if value.stip() == "":
+                    value = default
+            else:
+                value = input(prop["name"]+"\n"+str(choices)+"\n:")
+                if value not in choices:
+                    value = None
+                    print("The value must be in the set of choices")
+        return value
+            
+    def _process_text(default):
+        value = None
+        while value is None:
+            if default is not None:
+                value = input(prop["name"]+"("+prop["default"]+"): ")
+                if value.strip() == "":
+                    value = prop["default"]
+            else:
+                value = input(prop["name"]+": ")
+                if value.strip() == "":
+                    continue
+        return value
+
+
     @staticmethod
     def _process_prop(prop):
 
@@ -29,45 +75,12 @@ class PropertyReader:
 
         value = None
         if binary:
-
-            while value is None:
-                if default in ("n", "y"):
-                    value = input(prop["name"] + "[y/n]("+default+") :")
-                    if value.stip() == "":
-                        value = default
-                else:
-                    value = input(prop["name"] + "[y/n]:")
-                if value not in ("y", "n"):
-                    value = None
-                    print("Please type \'y\' or \'n\' ")
-
+            return PropertyReader._process_binary(default)
         elif choices is not None:
-
-            while value is None:
-                if default in choices:
-                    value = input(prop["name"]
-                                  + "\n[" + str(choices)
-                                  + "]\n(" + default
-                                  + ")\n:")
-                    if value.stip() == "":
-                        value = default
-                else:
-                    value = input(prop["name"]+"\n"+str(choices)+"\n:")
-                    if value not in choices:
-                        value = None
-                        print("The value must be in the set of choices")
-
+            return PropertyReader._process_choices(default, choices)
         else:
-
-            while value is None:
-                if default is not None:
-                    value = input(prop["name"]+"("+prop["default"]+"): ")
-                    if value.strip() == "":
-                        value = prop["default"]
-                else:
-                    value = input(prop["name"]+": ")
-                    if value.strip() == "":
-                        continue
+            return PropertyReader._process_text(default)
+            
         return value
 
     def load_properties(self, props):
