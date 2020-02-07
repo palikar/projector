5b929d1eb0f500dbc2db21047d9f1050f461cb33
Stanislav Arnaudov <stanislav_ts@abv.bg>
2020-02-07 01:11:21 +0100

    Adding auto completion for the choices

--- projector/property_reader.py
+++ projector/property_reader.py
@@ -6,6 +6,33 @@ readline.parse_and_bind('tab: complete')
 readline.parse_and_bind('set editing-mode emacs')
 
 
+class OptionsCompleter(object):
+
+    def __init__(self, options):
+        self.options = sorted(options)
+        return
+
+    def complete(self, text, state):
+        response = None
+        if state == 0:
+            # This is the first time for this text, so build a match list.
+            if text:
+                self.matches = [s
+                                for s in self.options
+                                if s and s.startswith(text)]
+            else:
+                self.matches = self.options[:]
+
+        # Return the state'th item from the match list,
+        # if we have that many.
+        try:
+            response = self.matches[state]
+        except IndexError:
+            response = None
+
+        return response
+
+
 class PropertyReader:
 
     def __init__(self):
@@ -35,6 +62,8 @@ class PropertyReader:
         return value == "y" or value == "yes"
 
     def _process_choices(prop, default, choices):
+        readline.set_completer(
+            OptionsCompleter(choices).complete)
         value = None
         while value is None:
             if default in choices:
@@ -49,6 +78,8 @@ class PropertyReader:
                 if value not in choices:
                     value = None
                     print("The value must be in the set of choices")
+
+        readline.set_completer(None)
         return value
 
     def _process_text(prop, default):
